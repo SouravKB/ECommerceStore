0bad2c1a307c31ed1cf8e0fd57cb63e232d9cb2b
SouravKB <souravkannanthab3219@gmail.com>
2021-12-06 00:59:36 +0530

    Edited firestore models.

--- lib/models/firestore/shop.dart
+++ lib/models/firestore/shop.dart
@@ -8,27 +8,25 @@ class Shop {
   @DocumentKey()
   final String shopId;
   final List<String> ownerIds;
-  final String shopPic;
-  final String category;
+  final String shopPicUrl;
+  final String type;
   final List<String> emailIds;
   final List<String> phoneNos;
   final String address;
   final int openTime;
   final int closeTime;
   final bool isOpen;
-  final List<String> orderIds;
 
   Shop({
     required this.shopId,
     required this.ownerIds,
-    required this.shopPic,
-    required this.category,
+    required this.shopPicUrl,
+    required this.type,
     required this.emailIds,
     required this.phoneNos,
     required this.address,
     required this.openTime,
     required this.closeTime,
     required this.isOpen,
-    required this.orderIds,
   });
 }
--- lib/models/firestore/shop.g.dart
+++ lib/models/firestore/shop.g.dart
@@ -10,42 +10,38 @@ extension ShopCfs on Shop {
   static const docShop = "Shop";
   static const keyShopId = "shopId";
   static const keyOwnerIds = "ownerIds";
-  static const keyShopPic = "shopPic";
-  static const keyCategory = "category";
+  static const keyShopPicUrl = "shopPicUrl";
+  static const keyType = "type";
   static const keyEmailIds = "emailIds";
   static const keyPhoneNos = "phoneNos";
   static const keyAddress = "address";
   static const keyOpenTime = "openTime";
   static const keyCloseTime = "closeTime";
   static const keyIsOpen = "isOpen";
-  static const keyOrderIds = "orderIds";
 
-  Map<String, Object?> toMap() =>
-      {
+  Map<String, Object?> toMap() => {
         keyShopId: shopId,
         keyOwnerIds: ownerIds,
-        keyShopPic: shopPic,
-        keyCategory: category,
+        keyShopPicUrl: shopPicUrl,
+        keyType: type,
         keyEmailIds: emailIds,
         keyPhoneNos: phoneNos,
         keyAddress: address,
         keyOpenTime: openTime,
         keyCloseTime: closeTime,
         keyIsOpen: isOpen,
-        keyOrderIds: orderIds,
       };
 
   static Shop fromSnapshot(DocumentSnapshot<Map<String, Object?>> snap) => Shop(
     shopId: snap.id,
         ownerIds: snap[keyOwnerIds] as List<String>,
-        shopPic: snap[keyShopPic] as String,
-        category: snap[keyCategory] as String,
+        shopPicUrl: snap[keyShopPicUrl] as String,
+        type: snap[keyType] as String,
         emailIds: snap[keyEmailIds] as List<String>,
         phoneNos: snap[keyPhoneNos] as List<String>,
         address: snap[keyAddress] as String,
         openTime: snap[keyOpenTime] as int,
         closeTime: snap[keyCloseTime] as int,
         isOpen: snap[keyIsOpen] as bool,
-        orderIds: snap[keyOrderIds] as List<String>,
       );
 }
