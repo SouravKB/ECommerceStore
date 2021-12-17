6f5f445aee286b94b7a4dca3db1d6e05050342cf
Tirumalesh <tirunaik678@gmail.com>
2021-12-17 16:48:07 +0530

    Edit user profile.

--- lib/ui/profile_pic.dart
+++ lib/ui/profile_pic.dart
@@ -1,46 +1,57 @@
-import 'package:ecommercestore/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfilePic extends StatelessWidget {
-  const ProfilePic({
+   const ProfilePic({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
-      width: 200,
-      child: Stack(
-        fit: StackFit.expand,
-        clipBehavior: Clip.none,
-        children: [
-           CircleAvatar(
-            backgroundImage: AssetImage("assets/imgs/test.jpeg"),
-          ),
-          Positioned(
-            right: -16,
-            bottom: 10,
-            child: SizedBox(
-              height: 46,
-              width: 46,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    side: BorderSide(color: Colors.white),
-                  ),
-                  primary: Colors.white,
-                  backgroundColor: Color(0xFFF5F6F9),
-                ),
-                onPressed: () {},
-                child: SvgPicture.asset("assets/imgs/test.jpeg"),
+    return Column(
+      children: [
+        SizedBox(
+          height: 200,
+          width: 200,
+          child: Stack(
+            fit: StackFit.expand,
+            clipBehavior: Clip.none,
+            children: [
+               Center(
+                 child: Stack(
+                   children: <Widget>[ CircleAvatar(
+                      radius: 70,
+                     child: ClipOval(child:Image.asset("assets/imgs/test.jpeg",height: 150,width: 150, fit: BoxFit.cover,),),
               ),
-            ),
-          )
-        ],
-      ),
+              Positioned(
+                  right: 1,
+                  bottom: 1,
+                  child: SizedBox(
+                    height: 46,
+                    width: 46,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                          side: BorderSide(color: Colors.white),
+                        ),
+                        primary: Colors.white,
+                        backgroundColor: Color(0xFFF5F6F9),
+                      ),
+                      onPressed: () {},
+                      child: Icon(Icons.camera_alt,color: Colors.black,),
+
+                    ),
+                  ),
+              )
+  ]
+                 ),
+               ),
+            ],
+          ),
+        ),
+        Text('profile name',style: TextStyle(fontSize:20,color: Colors.black),)
+      ],
     );
   }
+
 }
